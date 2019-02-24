3ef9e4242518d1c6002d55387e4be1901f5d8d8f
jamador47 <jamador@unitec.edu>
2019-02-24 13:58:20 -0600

    Ya existe tabla de migraciones

---
+++ database/migrations/2014_10_12_000000_create_errors_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateErrorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('errors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('O19')->nullable();
+            $table->integer('I5')->nullable();
+            $table->integer('I22')->nullable();
+            $table->integer('I21')->nullable();
+            $table->integer('I18')->nullable();
+            $table->integer('I10')->nullable();
+            $table->integer('I11')->nullable();
+            $table->integer('I1')->nullable();
+            $table->integer('I31')->nullable();
+            $table->integer('I30')->nullable();
+           $table->integer('I29')->nullable();
+            $table->integer('I28')->nullable();
+            $table->integer('I2')->nullable();
+            $table->integer('I27')->nullable();
+            $table->integer('O343')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('errors');
+    }
+}
