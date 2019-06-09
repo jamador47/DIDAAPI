a9f9ca1de668b91164f66ed63cf8b344393dc84b
jamador47 <jamador@unitec.edu>
2019-06-09 16:20:07 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,7 @@ Route::get('/iniciartablach', [
         ]);
     
 
-    Route::get('/postpos/{a1f1}&{a1f2}&{a1l1}&{a2f1}&{a2f2}&{a2l1}&{a2l2}&{y1f1}&{y1f2}&{y1l1}&{y2f1}&{y2f2}&{y2l1}&{y2l2}', [
+    Route::get('/postpos/{a1f1}&{a1f2}&{a1l1}&{a2f1}&{a2f2}&{a2l1}&{a2l2}&{y1f1}&{y1f2}&{y1l1}&{y2f1}&{y2f2}&{y2l1}&{y2l2}&{entregaon}', [
         'as' => 'post.pos',
             'uses' => 'posicionadorescontroller@update',
             'middleware' => 'cors',
