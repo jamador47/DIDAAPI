043e4cffb6e4f8d067edf0eeccd7c2aa572624b7
jamador47 <jamador@unitec.edu>
2019-09-27 12:04:03 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -122,8 +122,9 @@ Route::get('/api/cambiarorigenes/{x1}/{y1}/{a1}/{z1}/{x2}/{y2}/{a2}/{z2}/{x3}/{y
         'as' => 'obtener.origenes',
             'uses' => 'origenescontroller@getjson',
         'middleware' => 'cors',]);
-        Route::get('/api/obtenerorigenes2/', [
-            'as' => 'obtener.origenes',
+
+        Route::get('/api/obtenerorigenes2', [
+            'as' => 'obtener5.origenes',
                 'uses' => 'origenescontroller@getjson',
             'middleware' => 'cors',]);
         
